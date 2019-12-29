b23fab5db972eede51922b1e58e9fbef998bccab
WoL <>
2019-12-29 15:53:34 +0900

    https9

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,28 +23,34 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot(UrlGenerator $url)
+
+    public function boot()
+    //public function boot(UrlGenerator $url)
     {
         if (config('app.env') !== 'production'){
             \DB::listen(function($query){
                 \Log::info("Query Time:{$query->time}s] $query->sql");
             });
         }
+        if (request()->isSecure()) {// 本番環境(Heroku)でhttpsを強制する
+            \URL::forceScheme('https');
+        }
+        /*
         elseif (env('REDIRECT_HTTPS')) {// 本番環境(Heroku)でhttpsを強制する
             $url->forceScheme('https');
         }
         elseif (app()->environment('production')) {// 本番環境(Heroku)でhttpsを強制する
             $url->forceScheme('https');
         }
-        elseif (request()->isSecure()) {// 本番環境(Heroku)でhttpsを強制する
-            \URL::forceScheme('https');
-        }
+        
         elseif (config('app.env') === 'production'){// 本番環境(Heroku)でhttpsを強制する
             \URL::forceScheme('https');
         }
         elseif (\App::environment('production')){// 本番環境(Heroku)でhttpsを強制する 
             \URL::forceScheme('https');
         }
+        */
+
         Schema::defaultStringLength(191);
     }
 }
