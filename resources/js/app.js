4560c992233e4edaec1a69e566ffd3090e6cc135
WoL <>
2019-12-31 19:16:09 +0900

    axios.defaults.baseURL = `${window.location.origin}`;

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,7 +26,8 @@ Vue.use(VueRouter) // VueRouterプラグインを使用する これによって
 Vue.use(VueAxios, axios)
 //axios.defaults.baseURL = `${process.env.MIX_APP_URL}/api/v1`
 //axios.defaults.baseURL = `${process.env.API_BASE_URL}/api/v1`;
-axios.defaults.baseURL = `${window.location.origin}/api/v1`;
+//axios.defaults.baseURL = `${window.location.origin}/api/v1`;
+axios.defaults.baseURL = `${window.location.origin}`;
 Vue.use(VueAuth, auth)
 // Load Index
 Vue.component('index', Index) // ルートコンポーネントをインポートする
