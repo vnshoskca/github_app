fe976c3fca361c12b9773c3b702a6a63fce68cd7
WoL <>
2020-01-04 17:40:13 +0900

    axios.defaults.baseURL = `/api/v1`;

--- resources/js/app.js
+++ resources/js/app.js
@@ -26,7 +26,8 @@ Vue.use(VueRouter) // VueRouterプラグインを使用する これによって
 Vue.use(VueAxios, axios)
 //axios.defaults.baseURL = `${process.env.MIX_APP_URL}/api/v1`
 //axios.defaults.baseURL = `${process.env.MIX_BASE_URL}/api/v1`;
-axios.defaults.baseURL = process.env.MIX_BASE_URL;
+axios.defaults.baseURL = `/api/v1`;
+//axios.defaults.baseURL = process.env.MIX_BASE_URL;
 //axios.defaults.baseURL = `${process.env.MIX_BASE_URL}` + `/api/v1`;
 //axios.defaults.baseURL = `${window.location.origin}/api/v1`;
 
