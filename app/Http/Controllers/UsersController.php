88b0cf553602dfff5a357619c856a90c51c15617
WoL <>
2020-01-05 17:08:56 +0900

    image

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -53,7 +53,7 @@ class UsersController extends Controller {
             $user->update(['image' => '/storage/'.$image_name]);
             return response()->json($user);
             */
-            $file = $request->file('file');
+            $file = $request->file('image');
             // 第一引数はディレクトリの指定
             // 第二引数はファイル
             // 第三引数はpublickを指定することで、URLによるアクセスが可能となる
